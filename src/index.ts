649a21f9029694502f74b96940f2272d43dec4df
Caleb Meredith <calebmeredith8@gmail.com>
2017-01-22 16:38:07 -0500

    export types from object module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,51 @@
-export { StrongGraphQLType, StrongGraphQLInputType, StrongGraphQLOutputType, StrongGraphQLInputOutputType } from './type'
-export { createScalarType } from './scalar'
-export { createObjectType, StrongGraphQLObjectType } from './object'
-export { createEnumType } from './enum'
-export { createListType } from './list'
-export { createNullableType } from './nullable'
-export { wrapWeakType, IntegerType, FloatType, StringType, BooleanType, IDType } from './wrap'
-export { createSchema } from './schema'
-export { trimDescription } from './description'
+export {
+  StrongGraphQLType,
+  StrongGraphQLInputType,
+  StrongGraphQLOutputType,
+  StrongGraphQLInputOutputType,
+} from './type'
+
+export {
+  createScalarType,
+} from './scalar'
+
+export {
+  createObjectType,
+  StrongGraphQLObjectTypeConfig,
+  StrongGraphQLObjectType,
+  StrongGraphQLNullableObjectType,
+  StrongGraphQLArgsConfig,
+  StrongGraphQLArgConfig,
+  StrongGraphQLFieldConfig,
+  StrongGraphQLFieldConfigWithoutArgs,
+  StrongGraphQLFieldConfigWithArgs,
+} from './object'
+
+export {
+  createEnumType,
+} from './enum'
+
+export {
+  createListType,
+} from './list'
+
+export {
+  createNullableType,
+} from './nullable'
+
+export {
+  wrapWeakType,
+  IntegerType,
+  FloatType,
+  StringType,
+  BooleanType,
+  IDType,
+} from './wrap'
+
+export {
+  createSchema,
+} from './schema'
+
+export {
+  trimDescription,
+} from './description'
